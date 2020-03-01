49d508732d9e3f3b5c2ef8d87ff9e49b100ea26d
furuhama <furuhama@users.noreply.github.com>
2020-03-01 23:50:18 +0900

    Add comments

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -10,7 +10,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Debugging Logger
         android.util.Log.d("MainActivity", "hello world");
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -18,10 +20,13 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener(listener)
     }
 
+    // btn onClick event handler
     val listener = object:View.OnClickListener {
         override fun onClick(v: View?) {
+            // Display toast text
             Toast.makeText(this@MainActivity, "clicked!", Toast.LENGTH_SHORT).show()
 
+            // Inclement count
             val countTextView = findViewById<TextView>(R.id.count)
             var count = countTextView.text.toString().toInt()
             count++
